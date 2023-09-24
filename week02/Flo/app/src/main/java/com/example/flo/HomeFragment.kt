6638b5210c840302f4ce364c1d3f6c1ff27ae682
dokyung-kang <kangdk02@naver.com>
2023-09-24 19:48:09 +0900

    [2주차] Fragment 전환

--- week02/Flo/app/src/main/java/com/example/flo/HomeFragment.kt
+++ week02/Flo/app/src/main/java/com/example/flo/HomeFragment.kt
@@ -18,6 +18,10 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+        binding.homeTodayMusicImg1Iv.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm, AlbumFragment()).commitAllowingStateLoss()
+        }
+
         return binding.root
     }
 }
