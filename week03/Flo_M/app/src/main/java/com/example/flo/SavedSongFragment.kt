9f0fed45864faca378ccb45dc97e92a9f5e1bff9
dokyung-kang <kangdk02@naver.com>
2023-09-29 13:54:16 +0900

    [3주차] 미션2

---
+++ week03/Flo_M/app/src/main/java/com/example/flo/SavedSongFragment.kt
@@ -0,0 +1,24 @@
+package com.example.flo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flo.databinding.FragmentDetailBinding
+import com.example.flo.databinding.FragmentSavedsongBinding
+import com.example.flo.databinding.FragmentVideoBinding
+
+class SavedSongFragment : Fragment() {
+
+    lateinit var binding : FragmentSavedsongBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSavedsongBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
