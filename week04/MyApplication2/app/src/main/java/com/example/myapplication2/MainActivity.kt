c050a5f9ac9d0855f53c0becf73de4c5a7c99ab9
dokyung-kang <kangdk02@naver.com>
2023-10-06 01:03:13 +0900

    [4주차] 워커스레드에서 뷰랜더링 간단 실습 - Handler

---
+++ week04/MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.myapplication2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.myapplication2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+        // 핸들러 만들어줌
+        val handler = Handler(Looper.getMainLooper())
+        
+        val imageList = arrayListOf<Int>()
+
+        imageList.add(R.drawable.bear)
+        imageList.add(R.drawable.duke)
+        imageList.add(R.drawable.just)
+        imageList.add(R.drawable.bear)
+        imageList.add(R.drawable.duke)
+        imageList.add(R.drawable.just)
+        imageList.add(R.drawable.bear)
+        imageList.add(R.drawable.duke)
+        imageList.add(R.drawable.just)
+
+        // Main thread에서만 뷰렌더링 가능 but work thread에서 시도하면 에러남
+        // 1. 핸들러 만들어야 함
+        Thread {
+            for(image in imageList){
+                handler.post{
+                    // 여기서 작성한 코드가 Main thread로 전달되어 뷰렌더링 작업이 Main thread에서 오류없이 실행됨
+                    binding.iv.setImageResource(image)
+                }
+                Thread.sleep(2000)
+            }
+        }.start()
+
+    }
+}
