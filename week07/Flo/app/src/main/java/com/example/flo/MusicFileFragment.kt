834eb6546b07baa4168af3504f4c54da6d7a30e4
dokyung-kang <kangdk02@naver.com>
2023-11-08 00:12:26 +0900

    [7주차] 6주차 세팅

---
+++ week07/Flo/app/src/main/java/com/example/flo/MusicFileFragment.kt
@@ -0,0 +1,52 @@
+package com.example.flo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flo.databinding.FragmentDetailBinding
+import com.example.flo.databinding.FragmentMusicfileBinding
+import com.example.flo.databinding.FragmentVideoBinding
+
+class MusicFileFragment : Fragment() {
+
+    lateinit var binding : FragmentMusicfileBinding
+    private var musicFileDatas = ArrayList<MusicFile>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMusicfileBinding.inflate(inflater, container, false)
+
+        musicFileDatas.apply {
+            add(MusicFile("Butter", "방탄소년단 (BTS)", R.drawable.img_album_exp))
+            add(MusicFile("Lilac", "아이유 (IU)", R.drawable.img_album_exp2))
+            add(MusicFile("Next Level", "에스파 (AESPA)", R.drawable.img_album_exp3))
+            add(MusicFile("Boy with Luv", "방탄소년단 (BTS)", R.drawable.img_album_exp4))
+            add(MusicFile("BBoom BBoom", "모모랜드 (MOMOLAND)", R.drawable.img_album_exp5))
+            add(MusicFile("Weekend", "태연 (Tae Yeon)", R.drawable.img_album_exp6))
+            add(MusicFile("Butter", "방탄소년단 (BTS)", R.drawable.img_album_exp))
+            add(MusicFile("Lilac", "아이유 (IU)", R.drawable.img_album_exp2))
+            add(MusicFile("Next Level", "에스파 (AESPA)", R.drawable.img_album_exp3))
+            add(MusicFile("Boy with Luv", "방탄소년단 (BTS)", R.drawable.img_album_exp4))
+            add(MusicFile("BBoom BBoom", "모모랜드 (MOMOLAND)", R.drawable.img_album_exp5))
+            add(MusicFile("Weekend", "태연 (Tae Yeon)", R.drawable.img_album_exp6))
+        }
+
+        // 더미데이터랑 Adapter 연결
+        val musicFileRVAdapter = MusicFileRVAdapter(musicFileDatas)
+        // 리사이클러뷰에 어댑터를 연결
+        binding.musicfileRv.adapter = musicFileRVAdapter
+
+        musicFileRVAdapter.setMyItemClickListener(object : MusicFileRVAdapter.MyItemClickListener{
+            override fun onRemoveMusicFile(position: Int) {
+                musicFileRVAdapter.removeItem(position)
+            }
+        })
+
+        return binding.root
+    }
+}
---
+++ week07/Flo/app/src/main/java/com/example/flo/SavedSongFragment.kt
@@ -0,0 +1,64 @@
+package com.example.flo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flo.databinding.FragmentDetailBinding
+import com.example.flo.databinding.FragmentSavedsongBinding
+import com.example.flo.databinding.FragmentVideoBinding
+import com.google.gson.Gson
+
+class SavedSongFragment : Fragment() {
+
+    lateinit var binding : FragmentSavedsongBinding
+    private var saveSongDatas = ArrayList<SaveSong>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSavedsongBinding.inflate(inflater, container, false)
+
+        saveSongDatas.apply {
+            add(SaveSong("Butter", "방탄소년단 (BTS)", R.drawable.img_album_exp))
+            add(SaveSong("Lilac", "아이유 (IU)", R.drawable.img_album_exp2))
+            add(SaveSong("Next Level", "에스파 (AESPA)", R.drawable.img_album_exp3))
+            add(SaveSong("Boy with Luv", "방탄소년단 (BTS)", R.drawable.img_album_exp4))
+            add(SaveSong("BBoom BBoom", "모모랜드 (MOMOLAND)", R.drawable.img_album_exp5))
+            add(SaveSong("Weekend", "태연 (Tae Yeon)", R.drawable.img_album_exp6))
+            add(SaveSong("Butter", "방탄소년단 (BTS)", R.drawable.img_album_exp))
+            add(SaveSong("Lilac", "아이유 (IU)", R.drawable.img_album_exp2))
+            add(SaveSong("Next Level", "에스파 (AESPA)", R.drawable.img_album_exp3))
+            add(SaveSong("Boy with Luv", "방탄소년단 (BTS)", R.drawable.img_album_exp4))
+            add(SaveSong("BBoom BBoom", "모모랜드 (MOMOLAND)", R.drawable.img_album_exp5))
+            add(SaveSong("Weekend", "태연 (Tae Yeon)", R.drawable.img_album_exp6))
+        }
+
+        // 더미데이터랑 Adapter 연결
+        val saveSongRVAdapter = SaveSongRVAdapter(saveSongDatas)
+        // 리사이클러뷰에 어댑터를 연결
+        binding.saveSongRv.adapter = saveSongRVAdapter
+
+        saveSongRVAdapter.setMyItemClickListener(object : SaveSongRVAdapter.MyItemClickListener{
+            override fun onRemoveSaveSong(position: Int) {
+                saveSongRVAdapter.removeItem(position)
+            }
+        })
+
+        return binding.root
+    }
+    private fun changeSaveSongFragment(saveSong: SaveSong) {
+        (context as MainActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, AlbumFragment().apply {
+                arguments = Bundle().apply {
+                    val gson = Gson()
+                    val albumJson = gson.toJson(saveSong)
+                    putString("album", albumJson)
+                }
+            })
+            .commitAllowingStateLoss()
+    }
+}
