83b706fde325a4fe8b3169d32726d44428779f80
dokyung-kang <kangdk02@naver.com>
2023-11-25 01:36:46 +0900

    [9주차] 미션 - 추가 API 연동

--- week09/Flo_M/app/src/main/java/com/example/flo/Album.kt
+++ week09/Flo_M/app/src/main/java/com/example/flo/Album.kt
@@ -2,7 +2,6 @@ package com.example.flo
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.*
 
 
 @Entity(tableName = "AlbumTable")
@@ -10,5 +9,5 @@ data class Album(
     @PrimaryKey(autoGenerate = false) var id: Int = 0, // album의 pk는 임의로 지정해주기 위해 autogenerate 안씁니다.
     var title: String? = "",
     var singer: String? = "",
-    var coverImg: Int? = null
+    var coverImgUrl: Int? = null
 )
--- week09/Flo_M/app/src/main/java/com/example/flo/HomeFragment.kt
+++ week09/Flo_M/app/src/main/java/com/example/flo/HomeFragment.kt
@@ -10,10 +10,11 @@ import androidx.viewpager2.widget.ViewPager2
 import com.example.flo.databinding.FragmentHomeBinding
 import com.google.gson.Gson
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), AlbumView {
 
     lateinit var binding: FragmentHomeBinding
     private var albumDatas = ArrayList<Album>()
+    private lateinit var albumAdapter: AlbumRVAdapter
 
     private lateinit var songDB: SongDatabase
 
@@ -24,25 +25,6 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        // 데이터 리스트 생성 더미 데이터
-        songDB = SongDatabase.getInstance(requireContext())!!
-        albumDatas.addAll(songDB.albumDao().getAlbums()) // songDB에서 album list를 가져옵니다.
-        Log.d("albumlist", albumDatas.toString())
-
-        // 더미데이터랑 Adapter 연결
-        val albumRVAdapter = AlbumRVAdapter(albumDatas)
-        // 리사이클러뷰에 어댑터를 연결
-        binding.homeTodayMusicAlbumRv.adapter = albumRVAdapter
-
-        albumRVAdapter.setMyItemClickListener(object : AlbumRVAdapter.MyItemClickListener{
-            override fun onItemClick(album: Album) {
-                changeAlbumFragment(album)
-            }
-            override fun onRemoveAlbum(position: Int) {
-                albumRVAdapter.removeItem(position)
-            }
-        })
-
         val bannerAdapter = BannerVPAdapter(this)
         bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
         bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
@@ -52,18 +34,58 @@ class HomeFragment : Fragment() {
         bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
         binding.homeBannerVp.adapter = bannerAdapter
         binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
         return binding.root
     }
 
-    private fun changeAlbumFragment(album: Album) {
+    private fun changeAlbumFragment(albumChartSongs: AlbumChartSongs) {
         (context as MainActivity).supportFragmentManager.beginTransaction()
             .replace(R.id.main_frm, AlbumFragment().apply {
                 arguments = Bundle().apply {
                     val gson = Gson()
-                    val albumJson = gson.toJson(album)
+                    val albumJson = gson.toJson(albumChartSongs)
                     putString("album", albumJson)
                 }
             })
             .commitAllowingStateLoss()
     }
+
+    override fun onStart() {
+        super.onStart()
+        getAlbums()
+    }
+
+    private fun initRecyclerView(result: AlbumChartResult) {
+        albumAdapter = AlbumRVAdapter(requireContext(), result)
+
+        binding.homeTodayMusicAlbumRv.adapter = albumAdapter
+
+        albumAdapter.setMyItemClickListener(object : AlbumRVAdapter.MyItemClickListener{
+            override fun onItemClick(albumChartSongs: AlbumChartSongs) {
+                changeAlbumFragment(albumChartSongs)
+            }
+
+            override fun onRemoveAlbum(position: Int) {
+                albumAdapter.removeItem(position)
+            }
+        })
+    }
+
+    private fun getAlbums() {
+        val albumService = AlbumService()
+        albumService.setAlbumView(this)
+
+        albumService.getAlbums()
+
+    }
+
+    override fun onGetAlbumSuccess(code: Int, result: AlbumChartResult) {
+        initRecyclerView(result)
+    }
+
+    override fun onGetAlbumFailure(code: Int, message: String) {
+        Log.d("LOOK-FRAG/SONG-RESPONSE", message)
+    }
+
+
 }
