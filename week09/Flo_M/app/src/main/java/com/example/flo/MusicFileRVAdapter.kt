06305d17d81ed237af712deab3ec01fd9a68e52d
dokyung-kang <kangdk02@naver.com>
2023-11-24 21:16:41 +0900

    [9주차] 미션 초기 세팅

---
+++ week09/Flo_M/app/src/main/java/com/example/flo/MusicFileRVAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.flo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.flo.databinding.ItemMusicfileBinding
+import java.util.ArrayList
+
+class MusicFileRVAdapter (private val albumList: ArrayList<MusicFile>) : RecyclerView.Adapter<MusicFileRVAdapter.ViewHolder>(){
+
+    // 클릭 인터페이스 정의
+    interface MyItemClickListener{
+        //fun onItemClick(album: MusicFile)
+        fun onRemoveMusicFile(position: Int)
+    }
+
+    // 리스너 객체를 전달받는 함수랑 리스너 객체를 저장할 변수
+    private lateinit var mItemClickListener: MyItemClickListener
+
+    fun setMyItemClickListener(itemClickListener: MyItemClickListener){
+        mItemClickListener = itemClickListener
+    }
+
+    // 뷰홀더를 생성해줘야 할 때 호출되는 함수 => 아이템 뷰 객체를 만들어서 뷰홀더에 던져줍니다.
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): MusicFileRVAdapter.ViewHolder {
+        val binding: ItemMusicfileBinding = ItemMusicfileBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+
+        return ViewHolder(binding)
+    }
+
+    fun addItem(album: MusicFile){
+        albumList.add(album)
+        notifyDataSetChanged()
+    }
+
+    fun removeItem(position: Int){
+        albumList.removeAt(position)
+        notifyDataSetChanged()
+    }
+
+    // 뷰홀더에 데이터를 바인딩해줘야 할 때마다 호출되는 함수 => 엄청나게 많이 호출
+    override fun onBindViewHolder(holder: MusicFileRVAdapter.ViewHolder, position: Int) {
+        holder.bind(albumList[position])
+//        holder.itemView.setOnClickListener { mItemClickListener.onItemClick(albumList[position]) }
+        holder.binding.musicfileMore.setOnClickListener { mItemClickListener.onRemoveMusicFile(position) } //삭제됐을 때
+    }
+
+    // 데이터 세트 크기를 알려주는 함수 => 리사이클러뷰가 마지막이 언제인지를 알게 된다.
+    override fun getItemCount(): Int = albumList.size
+
+    // 뷰홀더
+    inner class ViewHolder(val binding: ItemMusicfileBinding): RecyclerView.ViewHolder(binding.root){
+
+        fun bind(album: MusicFile){
+            binding.musicfileTitle.text = album.title
+            binding.musicfileSinger.text = album.singer
+            binding.musicfileImg.setImageResource(album.coverImg!!)
+        }
+    }
+}
