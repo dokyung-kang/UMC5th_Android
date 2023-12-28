0002c080799a9ed295698e59181ed51f9df6fc55
dokyung-kang <kangdk02@naver.com>
2023-12-28 15:33:28 +0900

    FLO 클론코딩 10주차까지

--- FLO_Total/Flo/app/src/main/java/com/example/flo/ui/main/locker/LockerFragment.kt
+++ FLO_Total/Flo/app/src/main/java/com/example/flo/ui/main/locker/LockerFragment.kt
@@ -1,8 +1,7 @@
-package com.example.flo
+package com.example.flo.ui.main.locker
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
